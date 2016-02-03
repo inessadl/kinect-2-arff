bb9442966f13eebb9ba9c0d4d618d26b619c0a62
Inessa Luerce <idluerce@inf.ufpel.edu.br>
2016-02-03 18:04:37 -0200

    Revert "vitravius imported, button name changed"
    This reverts commit 9308c0e4bbd84fd3f1d7f3218c599fdcef9ce8e1.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,6 @@ namespace Microsoft.Samples.Kinect.BodyIndexBasics
     using System.Windows.Documents;
     using System.Windows.Input;
     using System.Windows.Navigation;
-    using LightBuzz.Vitruvius;
 
     //Beta
 
@@ -217,8 +216,8 @@ namespace Microsoft.Samples.Kinect.BodyIndexBasics
             string Label1 = Button1.Content.ToString();
             if (Label1.Equals("Gravar"))
             {
-                Button1.Content = "Salvar";
-                Button2.Content = "Cancelar";
+                Button1.Content = "Efetivar";
+                Button2.Content = "Descartar";
 
                 if (_firstFrame)
                 {
